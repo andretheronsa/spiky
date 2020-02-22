bcf81068984f3dc99905367cadc389b252b3adf2
andretheronsa <andretheronsa@gmail.com>
2020-02-22 15:06:00 +0200

    init

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ LABEL \
     "Email"="andretheronsa@gmail.com"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-    python3 \
-    python3-dev \
-    python3-pip
+    python3=3.7.3-1 \
+    python3-pip=18.1-5 \
+    && rm -rf /var/lib/apt/lists/*
 
-ADD requirements.txt requirements.txt
+COPY requirements.txt requirements.txt
 RUN python3 -m pip install -r requirements.txt
---
+++ spiky/app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+############################################################
+# Company: Kartoza                                         #
+# Project: Geopackage spike remover - interview test       #
+# Author: Andre Theron (andretheronsa@gmail.com)           #
+# Created: February 2020                                   #
+# Requires: Python version 3.4 for Pathlib                 #
+############################################################
