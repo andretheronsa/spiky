2d1cb7a50d9781bb1687197a74e3fcef136c48e0
andretheronsa <andretheronsa@gmail.com>
2020-02-23 14:12:36 +0200

    Major dev work to impliment algorythm

--- README.md
+++ README.md
@@ -7,43 +7,79 @@
 
 Program that removes spikes from a GeoPackage polygon
 
+## Overview
+
 ### Spike definition:
 * A single outlier vertex between two vertices that forms an acute angle to them
 * Removing the outlier should not change the polygon area significantly
 
 ### Notes:
-* A spike at the first vertex in a polygon is removed.
-* Spike could be inward or outward
+* A spike at the first vertex in a polygon is removed
+* A polygon can have more than one spike
+* Spikes could be inward or outward
+* Spikes can be on exterior of one or more interior boundries of the polygon
 * Geographical location should not affect spike detection (poles / equator)
 * Topology and geometry should be preserved - no simplifying
 
 ### Operation:
-* Accepts input *.gpkg file containing one, or more polygons
-* Iteratively checks for spikes by increasing the angle untill the max angle is reached or the polygon area changes
-* Optionally accepts 3 paramters
+* Accepts input *.gpkg file containing one, or more geometries
+* Iteratively checks for spikes on polygon geometries by increasing the angle until the max angle is reached or the polygon area changes significantly
+* Optionally accepts 2 paramters:
     - Max angle
     - Max area delta
 * Writes out *_despike.gpkg file with spikes removed to same folder as input
 
 ### Limitations
 * Does not work with line data
+* Does not consider geometries with dimensions higher than 2d
+* Input must be in geographic coordinates referenced to WGS84
 
 ## Getting started
-* Pull docker image (See below)
+
+### Docker
+
+The simplest way of using the tool is to run the Docker image
 
 The latest Docker image is available at: [Dockerhub](https://hub.docker.com/repository/docker/andretheronsa/spiky)
 
-Pull image with:
-
+1. Pull image with:
 ```shell
 docker pull andretheronsa/spiky:latest
 ```
-
-Run program with:
-
+2. Run program by mapping folder containing '.dpkg' files to be despiked as volume to /home/work/:
 ```shell
-docker run --rm spiky:latest -v :/home/
+docker run spiky:latest -v /home/user/input/:/home/input/
 ```
+
+3. Outputs will be written to the same folder with '_despiked' appended.
+
+### Local Python environment
+
+Alternatively the tool can be downloaded and run with Python (3.5+)
+Required modules should be installed with pip
+It is highly reccomended to use Python Virtual environments (or similar tool) to avoid conflicts
+Pip install on Windows will fail due to missing wheels for Geopandas dependencies - [alternative]()
+
+1. Pull code from git
+```shell
+git clone 
+```
+2. Enter folder and activate virtual environment (venv used in this case)
+```shell
+cd spiky/
+python -m venv venv
+./venv/Scripts/active
+
+```
+4. Install packages
+```shell
+python3 -m pip install -r requirements.txt
+```
+5. Run program with folder containing '.dpkg' files to be despiked
+```shell
+python3 spiky.py /home/user/files
+```
+6. Outputs will be written to the same folder with '_despiked' appended.
 
 ## Docs
 
@@ -51,7 +87,7 @@ Latest docs are available at: [Readthedocs](https://spiky.readthedocs.io/en/late
 
 ## Tests
 
-Testing is done with Pytest using Github actions: 
+Testing is done with Pytest and automated during master deploy using Github actions: 
 
 ## Author
 
--- spiky.py
+++ spiky.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser as arparse
 from glob import glob
 import geopandas as gpd
 from pathlib import Path
 import logging
-import sys
+from shapely import geometry
+from pygc import great_distance as gd
+
 
 '''
 Company: Kartoza
@@ -15,64 +18,163 @@ Requires: Python version 3.4+ for Pathlib
 '''
 
 
-def test_gpkg(gpkg: gpd) -> bool:
-    '''Test if GeoPackage is valid for de-spiking.'''
-    if gpkg.is_valid:
-        return True
-    else:
-        return False
+def cmd_line_parse():
+    '''Parse command line input and returns argument object'''
+
+    example = "example: python3 ./spiky.py /home/input/"
+    parser = arparse(description='Remove spikes from a GeoPackage polygons',
+                     epilog=example)
+    parser.add_argument('indir',
+                        type=str,
+                        default='/home/work/',
+                        help="Directory containing GeoPackage files")
+    parser.add_argument('-m',
+                        type=int,
+                        default=10,
+                        help="Maximum angle of spikes (deg)")
+    parser.add_argument('-a',
+                        type=int,
+                        default=1,
+                        help="Max area change after spike removal (%)")
+    args = parser.parse_args()
+    return(args)
 
 
-def calculate_angle(a: dict, b: dict, c: dict) -> float:
+def calculate_angle(a: geometry.point,
+                    b: geometry.point,
+                    c: geometry.point) -> float:
+    '''Calculate the absolute angle of point b towards point a and c
+        WGS84 Geographic coordinates required
+    '''
+    # Calculate heading (reverse azimuth) between ab and cb
+    ab = gd(start_latitude=a.y,
+            start_longitude=a.x,
+            end_latitude=b.y,
+            end_longitude=b.x)['reverse_azimuth']
+    cb = gd(start_latitude=c.y,
+            start_longitude=c.x,
+            end_latitude=b.y,
+            end_longitude=b.x)['reverse_azimuth']
+    b_angle = abs(ab - cb)
+    return b_angle
 
-    return angle
+
+def despike(polygon: geometry.polygon,
+            min_angle: int) -> geometry.polygon:
+    '''Removes vertices from shapely polygon with neighbouring angles > max'''
+
+    # Polygon always has a single exterior and can have multiple interior rings
+    coord_dict = {"exterior": [list(polygon.exterior.coords)]}
+    if len(list(polygon.interiors)) > 0:
+        coord_dict["interior"] = [i for i in list(polygon.interior.coords)]
+
+    # Iterate over outer and inner ring(s)
+    for side in coord_dict.items():
+        for linear_ring in coord_dict[side]:
+            # Add 2nd last point to list's 0th index to find 1st angle
+            linear_ring.insert(0, linear_ring[-2])
+
+            # Create dict of xy: angles - middle angle is calculated
+            angles_dict = {}
+            for i in range(len(linear_ring)-2):
+                point_a = geometry.point.Point(linear_ring[i])
+                point_b = geometry.point.Point(linear_ring[i+1])
+                point_c = geometry.point.Point(linear_ring[i+2])
+                b_angle = calculate_angle(point_a, point_b, point_c)
+                angles_dict[point_b] = b_angle
+
+            # Remove vertices with angles < angle
+            for vertex, angle in angles_dict.items():
+                if angle < min_angle:
+                    angles_dict.pop(vertex)
+
+        # Rebuild polygon
+        new_poly = angle
+    return new_poly
 
 
-def remove_spikes(gpkg: gpd, angle: int) -> gpd:
-    '''Removes vertices with neighbouring angles exceeding input angle'''
-    
-    # Remember to remove first element in polygon
-    return gpkg
+def optimize_angle(polygon: geometry.polygon,
+                   max_angle: int,
+                   max_area_delta: int) -> geometry.polygon:
+    '''Find spikes in a polygon.
 
+    Spikes are defined as a vertex forming a sharp angle
+    (up to specified max angle) with neighbouring vertices
+    and can be removed without significanbtly reducing the
+    polygon area.
+    '''
 
-def calc_area(gpkg: gpd) -> int:
-    '''Write a GeoPackage file.'''
-    area = gpkg.area
-    return area
-
-
-def iterate(gpkg: gpd, max_angle: int, max_area_delta: int):
     angle = 0
-    new_layers = []
-    for gpkg_layer in gpkg:
-        while angle < max_angle:
-            orig_area = calc_area(gpkg_layer)
-            new_gpkg = remove_spikes(gpkg, angle)
-            new_area = calc_area(new_gpkg)
-            area_delta = min(orig_area, new_area) / max(orig_area, new_area)
-            if area_delta > max_area_delta:
-                new_layers.append(orig_area)
+    while angle < max_angle:
+        orig_area = polygon.area
+        despiked_polygon = despike(polygon, angle)
+        new_area = despiked_polygon.area
+        area_delta = min(orig_area, new_area) / max(orig_area, new_area)
+        if area_delta > max_area_delta:
+            return polygon
+        else:
+            polygon = despiked_polygon
+            angle += 1
+
+
+def main(in_dir: str,
+         max_angle: int,
+         max_area_delta: int):
+    '''Main function
+    Handles file input and performs quality checks.
+    Finds all polygons from all GeoPackages.
+    Feeds polygons to the iterative despike subroutines.
+    Collects polygons and rebuilds geoseries and GeoPackages
+    Writes out de-spiked data.
+    '''
+
+    # Read all valid GeoPackages in in_dir
+    package_list = glob(in_dir + "*.gpkg")
+    for package_file in package_list:
+        package_path = Path(package_file).absolute()
+        package_gdf = gpd.read_file(package_path)
+
+        # Check geopackage contents
+        if any(package_gdf.is_empty):
+            logging.warning(f"{package_file} contains empty geometries-skip")
+            continue
+        elif not any(package_gdf.is_valid):
+            logging.warning(f"{package_file} contains invalid geometries-skip")
+            continue
+
+        # Ensure gpkg is geographic coordinates - to use great circle formulae
+        projection = package_gdf.crs
+        if not projection.is_geographic:
+            logging.warning(f"{package_file} is not in geographic coordinates")
+            continue
+        elif projection.name != 'WGS 84':
+            logging.warning(f"{package_file} uses datum other than WGS84")
+            continue
+
+        # Process all polygons in geopackage
+        new_package_list = []
+        for polygon in package_gdf["geometry"]:
+
+            # Despike polygon exterior and interior - ignore points/lines
+            if type(polygon) == geometry.polygon.Polygon:
+                despiked_layer = optimize_angle(polygon,
+                                                max_angle,
+                                                max_area_delta)
+                new_package_list.append(despiked_layer)
             else:
-                angle += 1
-    final_gpkg = new_layers  # Should append all layers
-    return final_gpkg
+                new_package_list(polygon)
 
+            # Rebuild geoseries
 
-def main(in_dir: str = '/home/input/',
-         max_angle: int = 10,
-         max_area_delta: int = 1):
-    '''Main program'''
-    gpkg_list = glob(in_dir + "*.gpkg")
-    for gpkg_file in gpkg_list:
-        gpkg_path = Path(gpkg_file)
-        gpkg_gpd = gpd.read_file(gpkg_path)
-        if not test_gpkg(gpkg_gpd):
-            logging.error(f"{gpkg_file} not a valid GeoPackage file for Spiky")
-            sys.exit(1)
-        despiked_gpkg = iterate(gpkg_gpd, max_angle, max_area_delta)
-        despiked_gpkg.tofile(gpkg_path.with_suffix("_despike.gpkg"),
-                             driver="GPKG")
+        # Rebuild GeoPackage - project back and save
+        despiked_gdf = new_package_list
+        despiked_gdf_proj = despiked_gdf.poject(projection)
+        despiked_gdf_proj.tofile(package_path.with_suffix("_despike.gpkg"),
+                                 driver="GPKG")
 
 
 if __name__ == "__main__":
-    main()
+    args = cmd_line_parse()
+    main(in_dir=args.in_dir,
+         max_angle=args.m,
+         max_area_delta=args.a)
