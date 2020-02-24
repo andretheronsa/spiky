6422328a28d78cc26d5bc5c9149e7e10b7fdbd08
andretheronsa <andretheronsa@gmail.com>
2020-02-24 18:24:19 +0200

    init

---
+++ tests/integration/test_spiky.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pytest
+
+target = __import__("spiky.py")
+spiky = target.spiky
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 4
+
+# orig_area = polygon.area
+# despiked_polygon = despike(polygon, angle)
+# new_area = despiked_polygon.area
+# area_delta = min(orig_area, new_area) / max(orig_area, new_area)
